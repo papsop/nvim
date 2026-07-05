7772b9a0ee2d58ed5ce2439d0b84581cfcab4b19
Patrik Papso (BlueX) <patrik.papso@warhorsestudios.cz>
2026-07-05 15:55:32 +0200

    Adding some more QoL keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -30,3 +30,19 @@ vim.keymap.set({ "n", "v" }, "D", '"_D')
 vim.keymap.set({ "n", "v" }, "x", '"_x')
 
 vim.keymap.set({ "n", "v" }, "<leader>d", "d")
+
+--- ThePrimeAgen keymaps from video
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
